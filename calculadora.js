36a52695036eb7db2f59f658e6c04601ffae1281
vnunxs <oerpvividossantos03@gmail.com>
2022-04-27 09:32:07 -0300

    first commit

---
+++ calculadora.js
@@ -0,0 +1,6 @@
+function StorageManager(){
+
+    let n1 = document.getElementById('n1').value;
+    let n2 = document.getElementById('n2').value;
+    document.getElementById('resultado').innerHTML = n1 + n2
+}
